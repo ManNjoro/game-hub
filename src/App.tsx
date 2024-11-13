041f95545c8b920097c252db0853dd6eb6871b90
Man Njoro <elijohnmwoho@gmail.com>
2024-11-13 23:46:41 +0300

    "Extracted Navbar component and added logo and text to it"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <Navbar />
         </GridItem>
         <Stack hideBelow="lg">
           <GridItem area="aside" bg="gold">
